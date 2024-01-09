ba8adcc9e9e5ea3ae169142ea65f697581fe5f4b
moqsien <moqsien@foxmail.com>
2024-01-09 13:17:26 +0800

    edge domains

--- pkgs/sites/domains.go
+++ pkgs/sites/domains.go
@@ -1,1 +1,42 @@
 package sites
+
+import (
+	"github.com/moqsien/proxy-collector/pkgs/confs"
+)
+
+const (
+	EdgeDomains SiteType = "edge_domains"
+)
+
+type EDomains struct {
+	result  []string
+	handler func([]string)
+	cnf     *confs.CollectorConf
+}
+
+func NewEDomains(cnf *confs.CollectorConf) (ed *EDomains) {
+	ed = &EDomains{
+		cnf:    cnf,
+		result: []string{},
+	}
+	return
+}
+
+func (e *EDomains) Type() SiteType {
+	return EdgeDomains
+}
+
+func (e *EDomains) SetHandler(h func([]string)) {
+	e.handler = h
+}
+
+func (e *EDomains) testDomains() {
+	// TODO: cloudflare SSL/TSL.
+}
+
+func (e *EDomains) Run() {
+	e.testDomains()
+	if e.handler != nil {
+		e.handler(e.result)
+	}
+}
