d50d83a78348dfdf1a174be421c01a36df7f3851
moqsien <moqsien@foxmail.com>
2024-02-27 22:00:43 +0800

    upda

--- pkgs/pxy/main.go
+++ pkgs/pxy/main.go
@@ -1,9 +1,20 @@
 package main
 
+import (
+	"github.com/gvcgo/proxy-collector/pkgs/confs"
+	"github.com/gvcgo/proxy-collector/pkgs/versions"
+)
+
 func main() {
 	// sites.TestEDomains()
 	// sites.TestEDCollector()
 	// sites.TestTDomains()
-	app := NewApp()
-	app.Run()
+
+	// app := NewApp()
+	// app.Run()
+
+	cfg := confs.NewCollectorConf()
+	gl := versions.NewGolang(cfg)
+	gl.FetchAll()
+	gl.Upload()
 }
