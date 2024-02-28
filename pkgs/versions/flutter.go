b4d1903a0dc1622864b129a8968a50c6af09709a
moqsien <moqsien@foxmail.com>
2024-02-28 23:03:15 +0800

    flutter

--- pkgs/versions/flutter.go
+++ pkgs/versions/flutter.go
@@ -1,1 +1,10 @@
 package versions
+
+const (
+	FlutterVersionFileName string = "flutter.version.json"
+)
+
+/*
+https://storage.googleapis.com/flutter_infra_release/releases/releases_{linux/macos/windows}.json
+https://storage.flutter-io.cn/flutter_infra_release/releases/releases_{linux/macos/windows}.json
+*/
