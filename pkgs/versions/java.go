32decdcd8bf4125ed4724c90fcab525aecf36d49
moqsien <moqsien@foxmail.com>
2024-03-04 14:40:52 +0800

    jdk

--- pkgs/versions/java.go
+++ pkgs/versions/java.go
@@ -3,16 +3,16 @@ package versions
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
-	"strings"
+	"time"
 
-	"github.com/PuerkitoBio/goquery"
-	"github.com/gvcgo/goutils/pkgs/gtea/gprint"
-	"github.com/gvcgo/goutils/pkgs/request"
 	"github.com/gvcgo/collector/pkgs/confs"
 	"github.com/gvcgo/collector/pkgs/upload"
 	"github.com/gvcgo/collector/pkgs/utils"
+	"github.com/gvcgo/goutils/pkgs/gtea/gprint"
+	"github.com/gvcgo/goutils/pkgs/request"
 )
 
 /*
@@ -21,12 +21,100 @@ https://injdk.cn/
 Latest versions only:
 https://www.oracle.com/cn/java/technologies/downloads/
 https://www.oracle.com/java/technologies/downloads/
+
+temurin:
+https://api.adoptium.net/v3/info/available_releases
+https://api.adoptium.net/v3/assets/latest/8/hotspot
 */
 
 const (
 	JavaVersionFileName string = "jdk.version.json"
-	OfficialHomepage    string = "https://www.oracle.com/java/technologies/downloads/"
+	AdoptiumURL         string = "https://api.adoptium.net/v3/info/available_releases"
+	AdoptiumAssetsURL   string = "https://api.adoptium.net/v3/assets/latest/%d/hotspot"
 )
+
+/*
+	{
+	    "available_lts_releases": [
+	        8,
+	        11,
+	        17,
+	        21
+	    ],
+	    "available_releases": [
+	        8,
+	        11,
+	        16,
+	        17,
+	        18,
+	        19,
+	        20,
+	        21
+	    ],
+	    "most_recent_feature_release": 21,
+	    "most_recent_feature_version": 23,
+	    "most_recent_lts": 21,
+	    "tip_version": 23
+	}
+*/
+type JdkAvailableVersions struct {
+	Releases []int `json:"available_releases"`
+}
+
+/*
+	{
+		"binary": {
+			"architecture": "x64",
+			"download_count": 590242,
+			"heap_size": "normal",
+			"image_type": "jdk",
+			"jvm_impl": "hotspot",
+			"os": "linux",
+			"package": {
+				"checksum": "fcfd08abe39f18e719e391f2fc37b8ac1053075426d10efac4cbf8969e7aa55e",
+				"checksum_link": "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u402-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u402b06.tar.gz.sha256.txt",
+				"download_count": 590242,
+				"link": "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u402-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u402b06.tar.gz",
+				"metadata_link": "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u402-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u402b06.tar.gz.json",
+				"name": "OpenJDK8U-jdk_x64_linux_hotspot_8u402b06.tar.gz",
+				"signature_link": "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u402-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u402b06.tar.gz.sig",
+				"size": 103003119
+			},
+			"project": "jdk",
+			"scm_ref": "jdk8u402-b06_adopt",
+			"updated_at": "2024-01-19T15:54:40Z"
+		},
+		"release_link": "https://github.com/adoptium/temurin8-binaries/releases/tag/jdk8u402-b06",
+		"release_name": "jdk8u402-b06",
+		"vendor": "eclipse",
+		"version": {
+			"build": 6,
+			"major": 8,
+			"minor": 0,
+			"openjdk_version": "1.8.0_402-b06",
+			"security": 402,
+			"semver": "8.0.402+6"
+		}
+	}
+*/
+type JPackage struct {
+	Checksum string `json:"checksum"`
+	Url      string `json:"link"`
+}
+
+type JBinary struct {
+	Arch      string    `json:"architecture"`
+	Os        string    `json:"os"`
+	ImageType string    `json:"image_type"`
+	Package   *JPackage `json:"package"`
+	CLib      string    `json:"c_lib"`
+}
+
+type JdkItem struct {
+	Binary      *JBinary `json:"binary"`
+	ReleaseName string   `json:"release_name"`
+	Vendor      string   `json:"vendor"`
+}
 
 type JDK struct {
 	cnf      *confs.CollectorConf
@@ -34,7 +122,6 @@ type JDK struct {
 	versions Versions
 	fetcher  *request.Fetcher
 	homepage string
-	doc      *goquery.Document
 }
 
 func NewJDK(cnf *confs.CollectorConf) (j *JDK) {
@@ -42,7 +129,7 @@ func NewJDK(cnf *confs.CollectorConf) (j *JDK) {
 		cnf:      cnf,
 		versions: Versions{},
 		fetcher:  request.NewFetcher(),
-		homepage: "https://injdk.cn/",
+		homepage: AdoptiumURL,
 		uploader: upload.NewUploader(cnf),
 	}
 	if confs.EnableProxyOrNot() {
@@ -55,171 +142,60 @@ func NewJDK(cnf *confs.CollectorConf) (j *JDK) {
 	return
 }
 
-func (j *JDK) GetFileSuffix(fName string) string {
-	var allowedSuffixes = []string{
-		".zip",
-		".tar.gz",
-		".tar.bz2",
-		".tar.xz",
+func (j *JDK) FetchAll() {
+	j.fetcher.Timeout = time.Second * 60
+	j.fetcher.SetUrl(j.homepage)
+	versionList := JdkAvailableVersions{
+		Releases: []int{},
 	}
-	for _, k := range allowedSuffixes {
-		if strings.HasSuffix(fName, k) {
-			return k
-		}
-	}
-	return ""
-}
-
-func (j *JDK) GetDoc(homepage string) {
-	j.fetcher.Url = homepage
 	if resp := j.fetcher.Get(); resp != nil {
-		j.doc, _ = goquery.NewDocumentFromReader(resp.RawBody())
-	}
-	if j.doc == nil {
-		gprint.PrintError(fmt.Sprintf("Cannot parse html for %s", j.fetcher.Url))
-		os.Exit(1)
-	}
-}
-
-func formatVersionName(vName string) string {
-	vName = strings.ToLower(vName)
-	vName = strings.ReplaceAll(vName, "\n", "")
-	vName = strings.ReplaceAll(vName, "\r", "")
-	vName = strings.ReplaceAll(vName, " ", "")
-	vName = strings.ReplaceAll(vName, "jdk", "")
-	return vName
-}
-
-func (j *JDK) GetVersionsFromInjdk() {
-	j.homepage = "https://injdk.cn/"
-	j.GetDoc(j.homepage)
-
-	j.doc.Find("div#oracle-jdk").Find("div.col-sm-3").Each(func(i int, s *goquery.Selection) {
-		vName := formatVersionName(strings.ToLower(s.Find("span").Text()))
-		var extraStr string
-		if strings.Contains(vName, "(lts)") {
-			vName = strings.ReplaceAll(vName, "(lts)", "")
-			extraStr = "LTS"
+		content, _ := io.ReadAll(resp.RawBody())
+		if err := json.Unmarshal(content, &versionList); err != nil {
+			gprint.PrintError(fmt.Sprintf("Parse content from %s failed.", j.fetcher.Url))
+			return
 		}
-		extraStr = strings.Join([]string{extraStr, "injdk.cn"}, ",")
+	}
 
-		s.Find("li").Each(func(i int, ss *goquery.Selection) {
-			if strings.Contains(vName, "jdk8") {
-				return
-			}
+OUTTER:
+	for _, vInt := range versionList.Releases {
+		vName := fmt.Sprintf("%d", vInt)
+		u := fmt.Sprintf(AdoptiumAssetsURL, vInt)
+		j.fetcher.SetUrl(u)
 
-			fileName := strings.ReplaceAll(strings.ToLower(ss.Find("a").Text()), " ", "")
-			ver := &VFile{}
-			ver.Arch = utils.ParseArch(fileName)
-			ver.Os = utils.ParsePlatform(fileName)
-			if ver.Arch == "" || ver.Os == "" {
-				return
+		vList := []*JdkItem{}
+		if resp := j.fetcher.Get(); resp != nil {
+			content, _ := io.ReadAll(resp.RawBody())
+			if err := json.Unmarshal(content, &vList); err != nil {
+				gprint.PrintError(fmt.Sprintf("Parse content from %s failed.", j.fetcher.Url))
+				continue OUTTER
 			}
-			if suffix := j.GetFileSuffix(fileName); suffix == "" {
-				return
-			}
-			ver.Url = strings.ReplaceAll(ss.Find("a").AttrOr("href", ""), " ", "")
-			if ver.Url == "" {
-				return
-			}
-
-			if vlist, ok := j.versions[vName]; !ok || vlist == nil {
-				j.versions[vName] = []*VFile{}
-			}
-
-			ver.Extra = extraStr
-			j.versions[vName] = append(j.versions[vName], ver)
-		})
-	})
-
-	j.doc.Find("#Kona").Find("div.col-sm-3").Each(func(i int, s *goquery.Selection) {
-		vName := formatVersionName(strings.ToLower(s.Find("span").Text()))
-		if strings.Contains(vName, "(lts)") {
-			vName = strings.ReplaceAll(vName, "(lts)", "")
 		}
-		extraStr := "LTS$injdk.cn"
-
-		s.Find("li").Each(func(i int, ss *goquery.Selection) {
-			if !strings.Contains(vName, "jdk8") {
-				return
+	INNER:
+		for _, item := range vList {
+			if item.Binary == nil || item.Binary.ImageType != "jdk" || item.Binary.Package == nil {
+				continue INNER
 			}
-
-			fileName := strings.ReplaceAll(strings.ToLower(ss.Find("a").Text()), " ", "")
-			ver := &VFile{}
-			ver.Arch = utils.ParseArch(fileName)
-			ver.Os = utils.ParsePlatform(fileName)
-			if ver.Arch == "" || ver.Os == "" {
-				return
-			}
-			if suffix := j.GetFileSuffix(fileName); suffix == "" {
-				return
-			}
-			ver.Url = strings.ReplaceAll(ss.Find("a").AttrOr("href", ""), " ", "")
-			if ver.Url == "" {
-				return
-			}
-
-			if vlist, ok := j.versions[vName]; !ok || vlist == nil {
-				j.versions[vName] = []*VFile{}
-			}
-			ver.Extra = extraStr
-			j.versions[vName] = append(j.versions[vName], ver)
-		})
-	})
-}
-
-func (j *JDK) GetSha(sUrl string) (res string) {
-	j.fetcher.Url = sUrl
-	res, _ = j.fetcher.GetString()
-	return
-}
-
-func (j *JDK) GetVersionsFromOfficial() {
-	j.homepage = OfficialHomepage
-	j.GetDoc(j.homepage)
-
-	j.doc.Find("ul.rw-inpagetabs").First().Find("li").Each(func(i int, s *goquery.Selection) {
-		v, _ := s.Find("a").Attr("href")
-		sList := strings.Split(v, "java")
-		vn := sList[len(sList)-1]
-		j.doc.Find(fmt.Sprintf("div#java%s", vn)).After("nav").Find("table").Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
-			if i == 0 {
-				return
-			}
-			tArchive := strings.ToLower(s.Find("td").Eq(0).Text())
-			tArchive = strings.ReplaceAll(tArchive, " ", "")
-			tUrl, _ := s.Find("td").Eq(2).Find("a").Eq(0).Attr("href")
-			// tSha, _ := s.Find("td").Eq(2).Find("a").Eq(1).Attr("href")
-			if !strings.Contains(tArchive, "archive") {
-				return
+			if item.Binary.Os == "alpine-linux" || item.Binary.CLib == "musl" {
+				continue INNER
 			}
 			ver := &VFile{}
-			ver.Arch = utils.ParseArch(tUrl)
-			ver.Os = utils.ParsePlatform(tUrl)
-			if ver.Arch == "" || ver.Os == "" || tUrl == "" {
-				return
-			}
-
-			ver.Url = tUrl
-			if suffix := j.GetFileSuffix(ver.Url); suffix == "" {
-				return
-			}
-			ver.Sum = j.GetSha(fmt.Sprintf("%s.%s", ver.Url, "sha256"))
+			ver.Url = item.Binary.Package.Url
+			ver.Sum = item.Binary.Package.Checksum
 			if ver.Sum != "" {
 				ver.SumType = "sha256"
 			}
-			if vlist, ok := j.versions[vn]; !ok || vlist == nil {
-				j.versions[vn] = []*VFile{}
+			ver.Arch = utils.ParseArch(item.Binary.Arch)
+			ver.Os = utils.ParsePlatform(item.Binary.Os)
+			if ver.Arch == "" || ver.Os == "" {
+				continue INNER
 			}
-			ver.Extra = "oracle.com"
-			j.versions[vn] = append(j.versions[vn], ver)
-		})
-	})
-}
-
-func (j *JDK) FetchAll() {
-	j.GetVersionsFromInjdk()
-	j.GetVersionsFromOfficial()
+			ver.Extra = fmt.Sprintf("%s$%s", item.ReleaseName, item.Vendor)
+			if vl, ok := j.versions[vName]; !ok || vl == nil {
+				j.versions[vName] = []*VFile{}
+			}
+			j.versions[vName] = append(j.versions[vName], ver)
+		}
+	}
 }
 
 func (j *JDK) Upload() {
